5121f444185e6e42083974ad54c8342b8deb6b97
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2019-04-26 16:55:25 +0430

    Add @local

--- api/src/main/java/com/javastudio/tutorial/api/PersonService.java
+++ api/src/main/java/com/javastudio/tutorial/api/PersonService.java
@@ -2,6 +2,9 @@ package com.javastudio.tutorial.api;
 
 import com.javastudio.tutorial.dto.PersonDTO;
 
+import javax.ejb.Local;
+
+@Local
 public interface PersonService extends GeneralServiceApi<PersonDTO> {
 
 }
